d185ce2ff4f57f0f3b1be4c5bd966c24e392a27d
Daniel Rivera <daniele.riverag@konradlorenz.edu.co>
2018-05-13 19:41:26 -0500

    the design of the view is generated for the shopping cart, the redundancy in the FoodDetail.java class is corrected

--- app/src/main/java/com/example/daniel/table_food/FoodDetail.java
+++ app/src/main/java/com/example/daniel/table_food/FoodDetail.java
@@ -4,11 +4,15 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.daniel.table_food.Database.Database;
 import com.example.daniel.table_food.Model.Food;
+import com.example.daniel.table_food.Model.Order;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,6 +33,8 @@ public class FoodDetail extends AppCompatActivity {
     String foodId="";
     FirebaseDatabase database;
     DatabaseReference foods;
+    Food currentFood;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +44,29 @@ public class FoodDetail extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         foods = database.getReference("Food");
 
-        numberButton=(ElegantNumberButton)findViewById(R.id.number_button);
-        btnCart = (FloatingActionButton)findViewById(R.id.btnCart);
-        Descfood=(TextView)findViewById(R.id.food_description);
-        Namefood=(TextView)findViewById(R.id.name_food);
-        Pricefood=(TextView)findViewById(R.id.food_price);
-        Imgfood=(ImageView)findViewById(R.id.img_sfood);
+        numberButton=findViewById(R.id.number_button);
+        btnCart = findViewById(R.id.btnCart);
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Database(getBaseContext()).addToCart(new Order(
+                        foodId,
+                        currentFood.getName(),
+                        numberButton.getNumber(),
+                        currentFood.getPrice(),
+                        currentFood.getDiscount()
 
-        collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
+                ));
+                Toast.makeText(FoodDetail.this, R.string.addCar,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Descfood=findViewById(R.id.food_description);
+        Namefood=findViewById(R.id.name_food);
+        Pricefood=findViewById(R.id.food_price);
+        Imgfood=findViewById(R.id.img_sfood);
+
+        collapsingToolbarLayout = findViewById(R.id.collapsing);
         collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
         collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.CollapseAppBar);
 
@@ -62,13 +83,13 @@ public class FoodDetail extends AppCompatActivity {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Food food = dataSnapshot.getValue(Food.class);
+                currentFood = dataSnapshot.getValue(Food.class);
 
-                Picasso.with(getBaseContext()).load(food.getImage()).into(Imgfood);
-                collapsingToolbarLayout.setTitle(food.getName());
-                Pricefood.setText(food.getPrice());
-                Namefood.setText(food.getName());
-                Descfood.setText(food.getDescription());
+                Picasso.with(getBaseContext()).load(currentFood.getImage()).into(Imgfood);
+                collapsingToolbarLayout.setTitle(currentFood.getName());
+                Pricefood.setText(currentFood.getPrice());
+                Namefood.setText(currentFood.getName());
+                Descfood.setText(currentFood.getDescription());
             }
 
             @Override
